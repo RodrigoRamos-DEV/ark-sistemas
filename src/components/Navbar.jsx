a5510cc27cd8cc5ff489ae28a83a278f677c50dd
Rodrigo Ramos <rodrigomramos18@gmail.com>
2025-07-27 15:57:43 -0300

    aces

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
-import logo from '../assets/SistemaARK.png'; // Importa a logo
+import logo from '../assets/SistemaARK.png';
 
 const Navbar = () => {
   return (
     <nav className="navbar">
       <div className="navbar-container">
         <img src={logo} alt="ARK Sistemas Logo" className="navbar-logo" />
-        <a href="#contato" className="navbar-button">
-          Fale Conosco
-        </a>
+        <div className="navbar-links">
+          <a href="#contato" className="navbar-button">
+            Fale Conosco
+          </a>
+          <a href="https://ark-pro-app.onrender.com" className="navbar-button">
+            Acesso ao Sistema
+          </a>
+        </div>
       </div>
     </nav>
   );
